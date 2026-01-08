c6feca87571bed348e89912bec70ab69b0d6398e
feburkart-jpg <feburkart@g.ucla.edu>
2026-01-08 15:37:23 -0800

    Add files via upload

---
+++ Burkart_HW0.m
@@ -0,0 +1,48 @@
+%% HW 0
+
+% Name: Felix Burkart
+% Date: 01/06/2026
+% Collaborators: Anna Fedetov (explained what %.2f does in print function)
+
+clear all
+close all
+clc
+
+
+%% Problem 1 - Triangle Perimeter
+% Write code that calculates and prints the perimeter of a triangle. 
+
+a = 3.2;
+%assign variable x a value of 3.2
+
+b = 5.1;
+%assign varible b a value of 5.1
+
+c = 4.3; 
+%assign c a variable of 4.3
+
+Perimeter = a+b+c;
+%assign variable 'Perimeter' as sum of a, b and c
+
+fprintf('Problem 1: %.2f m \n', Perimeter)
+%print 'problem 1:' followed by the variable perimeter as a float rounded
+%to the second decimal place 
+
+
+
+%% Problem 2 - Syllabus
+%{
+Grading Rubric: 
+   
+    Homework will be graded out of 10 points:
+        1 For header and name
+        2 points for style, 
+        7 points for execution and answers
+
+        
+
+File Name:  
+       Burkart_HW0.m 
+%}
+
+
